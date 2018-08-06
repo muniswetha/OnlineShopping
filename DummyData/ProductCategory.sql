199f12406d17f82ab0924080d50a1dca859fc16c
mokkm <muniswetha.mokkala@campsystems.com>
2018-08-06 12:18:26 +0530

    First Commit

---
+++ DummyData/ProductCategory.sql
@@ -0,0 +1,3 @@
+﻿INSERT INTO [dbo].[ProductCategory] ([Id], [Name], [Description], [CreatedDate]) VALUES (1, N'Furniture', N'Furniture', N'2018-07-24')
+INSERT INTO [dbo].[ProductCategory] ([Id], [Name], [Description], [CreatedDate]) VALUES (2, N'Office Supplies', N'Office Supplies', N'2018-07-24')
+INSERT INTO [dbo].[ProductCategory] ([Id], [Name], [Description], [CreatedDate]) VALUES (3, N'Technology', N'Technology', N'2018-07-24')
---
+++ DummyData/ProductSubCategory.sql
@@ -0,0 +1,29 @@
+﻿INSERT INTO [dbo].[ProductSubCategory] ([Id], [ProductCategoryId], [Name], [Description], [CreatedDate]) VALUES (1, 1, N'Bookcases
+', N'Bookcases
+', N'2018-07-24')
+INSERT INTO [dbo].[ProductSubCategory] ([Id], [ProductCategoryId], [Name], [Description], [CreatedDate]) VALUES (2, 1, N'Chairs
+', N'Chairs
+', N'2018-07-24')
+INSERT INTO [dbo].[ProductSubCategory] ([Id], [ProductCategoryId], [Name], [Description], [CreatedDate]) VALUES (3, 1, N'Tables
+', N'Tables
+', N'2018-07-24')
+INSERT INTO [dbo].[ProductSubCategory] ([Id], [ProductCategoryId], [Name], [Description], [CreatedDate]) VALUES (4, 1, N'Furnishings
+', N'Furnishings
+', N'2018-07-24')
+INSERT INTO [dbo].[ProductSubCategory] ([Id], [ProductCategoryId], [Name], [Description], [CreatedDate]) VALUES (5, 2, N'Appliances
+', N'Appliances
+', N'2018-07-24')
+INSERT INTO [dbo].[ProductSubCategory] ([Id], [ProductCategoryId], [Name], [Description], [CreatedDate]) VALUES (6, 2, N'Art', N'Art', N'2018-07-24')
+INSERT INTO [dbo].[ProductSubCategory] ([Id], [ProductCategoryId], [Name], [Description], [CreatedDate]) VALUES (7, 2, N'Binders', N'Binders', N'2018-07-24')
+INSERT INTO [dbo].[ProductSubCategory] ([Id], [ProductCategoryId], [Name], [Description], [CreatedDate]) VALUES (8, 2, N'Envelopes', N'Envelopes', N'2018-07-24')
+INSERT INTO [dbo].[ProductSubCategory] ([Id], [ProductCategoryId], [Name], [Description], [CreatedDate]) VALUES (9, 2, N'Fasteners', N'Fasteners', N'2018-07-24')
+INSERT INTO [dbo].[ProductSubCategory] ([Id], [ProductCategoryId], [Name], [Description], [CreatedDate]) VALUES (10, 2, N'Labels', N'Labels', N'2018-07-24')
+INSERT INTO [dbo].[ProductSubCategory] ([Id], [ProductCategoryId], [Name], [Description], [CreatedDate]) VALUES (11, 2, N'Paper', N'Paper', N'2018-07-24')
+INSERT INTO [dbo].[ProductSubCategory] ([Id], [ProductCategoryId], [Name], [Description], [CreatedDate]) VALUES (12, 2, N'Storage', N'Storage', N'2018-07-24')
+INSERT INTO [dbo].[ProductSubCategory] ([Id], [ProductCategoryId], [Name], [Description], [CreatedDate]) VALUES (13, 2, N'Supplies', N'Supplies', N'2018-07-24')
+INSERT INTO [dbo].[ProductSubCategory] ([Id], [ProductCategoryId], [Name], [Description], [CreatedDate]) VALUES (14, 3, N'Accessories
+', N'Accessories
+', N'2018-07-24')
+INSERT INTO [dbo].[ProductSubCategory] ([Id], [ProductCategoryId], [Name], [Description], [CreatedDate]) VALUES (15, 3, N'Copiers', N'Copiers', N'2018-07-24')
+INSERT INTO [dbo].[ProductSubCategory] ([Id], [ProductCategoryId], [Name], [Description], [CreatedDate]) VALUES (16, 3, N'Machines', N'Machines', N'2018-07-24')
+INSERT INTO [dbo].[ProductSubCategory] ([Id], [ProductCategoryId], [Name], [Description], [CreatedDate]) VALUES (17, 3, N'Phones', N'Phones', N'2018-07-24')
