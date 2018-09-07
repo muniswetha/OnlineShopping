3c03c2b141ad51e32ac5632fa6b9efb8c537f821
sharathdomakonda <sharath.domakonda@campsystems.com>
2018-09-07 11:24:45 +0530

    Created Class for User registration

---
+++ OnlineShoppingDataAcessLayer/Views/UserDetailsViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShoppingDataAcessLayer.Views
+{
+    public class UserDetailsViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public int PinCode { get; set; }
+        public string PhoneNo { get; set; }
+        public string AlternatePhoneNo { get; set; }
+        public int Fax { get; set; }
+        public int FirstSQId { get; set; }
+        public string FirstSQAnswer { get; set; }
+        public int SecondSQId { get; set; }
+        public string SecondSQAnswer { get; set; }
+        public int ThirdSQId { get; set; }
+        public string ThirdSQAnswer { get; set; }
+
+    }
+}
